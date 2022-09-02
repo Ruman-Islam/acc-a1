73640c85a9040943d441d90817be112bed63922a
Ruman-Islam <rumanislam48@gmail.com>
2022-09-03 02:20:37 +0600

    test

--- controllers/userInfo.controller.js
+++ controllers/userInfo.controller.js
@@ -3,12 +3,9 @@ const getRandomIndex = require('../utilities/randomIndexGen');
 
 module.exports.getAllUsers = (req, res, next) => {
     try {
-        // const users = fs.readFileSync("userData.json");
-        // const data = JSON.parse(users);
-        // res.status(200).json({ "data": data });
-        res.json({
-            "message": "hello test"
-        })
+        const users = fs.readFileSync("userData.json");
+        const data = JSON.parse(users);
+        res.status(200).send(data);
     } catch (err) {
         next('Failed to Read Data !!');
     }
