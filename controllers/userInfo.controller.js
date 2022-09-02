741d054938f05e69c8bd84d0bf44d91678667f08
Ruman-Islam <rumanislam48@gmail.com>
2022-09-03 02:33:38 +0600

    test

--- controllers/userInfo.controller.js
+++ controllers/userInfo.controller.js
@@ -3,11 +3,9 @@ const getRandomIndex = require('../utilities/randomIndexGen');
 
 module.exports.getAllUsers = (req, res, next) => {
     try {
-        const users = fs.readFileSync("userData.json");
-        res.write(users);
-        res.end();
-        // const data = JSON.parse(users);
-        // res.status(200).send(data);
+        const users = fs.readFileSync("./users.json", "utf-8");
+        const data = JSON.parse(users);
+        res.status(200).send(data);
     } catch (err) {
         next('Failed to Read Data !!');
     }
@@ -16,7 +14,7 @@ module.exports.getAllUsers = (req, res, next) => {
 
 module.exports.getRandomUser = (req, res, next) => {
     try {
-        const users = fs.readFileSync("userData.json");
+        const users = fs.readFileSync("./users.json");
         const data = JSON.parse(users);
         const i = getRandomIndex(data.length);
         res.status(200).json({ "data": data[i] });
@@ -24,3 +22,14 @@ module.exports.getRandomUser = (req, res, next) => {
         next('Failed to Read Data !!');
     }
 };
+
+// app.get("/user/all", (req, res) => {
+//     const users = fs.readFileSync("./users.json", "utf-8");
+//     const user = JSON.parse(users);
+//     const limit = req.query.limit;
+//     if (limit) {
+//         res.send(user.slice(0, limit));
+//     } else {
+//         res.send(user);
+//     }
+// }),
