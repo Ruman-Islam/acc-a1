20df73699bbe11cf41e8a73c334dad40685fd3e3
Ruman-Islam <rumanislam48@gmail.com>
2022-09-03 18:45:24 +0600

    bug fixed

--- controllers/userInfo.controller.js
+++ controllers/userInfo.controller.js
@@ -6,7 +6,7 @@ const getRandomIndex = require('../utilities/randomIndexGen');
 
 module.exports.getRandomUser = async (req, res, next) => {
     try {
-        const file = path.join(process.cwd(), './users.json');
+        const file = path.join(process.cwd(), './public/users.json');
         const data = await JSON.parse(fs.readFileSync(file, "utf-8"));
         const index = getRandomIndex(data.length);
         res.status(200).json({ "data": data[index] });
@@ -23,7 +23,7 @@ module.exports.getRandomUser = async (req, res, next) => {
 module.exports.getAllUsers = async (req, res, next) => {
     try {
         const { limit } = req.query;
-        const file = path.join(process.cwd(), './users.json');
+        const file = path.join(process.cwd(), './public/users.json');
         const data = await JSON.parse(fs.readFileSync(file, "utf-8"));
         const users = await limit > 0 ? data.slice(0, limit) : data;
         await res.status(200).json({ "data": users });
@@ -58,7 +58,7 @@ module.exports.updateUser = async (req, res, next) => {
     try {
         const { id } = req.params;
         const updatedData = req.body;
-        const file = path.join(process.cwd(), './users.json');
+        const file = path.join(process.cwd(), './public/users.json');
         const data = await JSON.parse(fs.readFileSync(file, "utf-8"));
         const newArray = await data.map(user => {
             if ((user.id).toString() === id) {
