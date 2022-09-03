3f668ff4178c715233244a97a18e00816a8924af
Ruman-Islam <rumanislam48@gmail.com>
2022-09-03 11:04:10 +0600

    bug fixed

--- controllers/userInfo.controller.js
+++ controllers/userInfo.controller.js
@@ -2,20 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const getRandomIndex = require('../utilities/randomIndexGen');
 
-module.exports.getAllUsers = (req, res, next) => {
-    try {
-        const file = path.join(process.cwd(), './users.jso');
-        const users = fs.readFileSync(file, "utf-8");
-        const data = JSON.parse(users);
-        res.status(200).json({ "data": data });
-    } catch (err) {
-        next({
-            status: 500,
-            success: false,
-            message: "Internal Server Error"
-        });
-    }
-};
 
 
 module.exports.getRandomUser = (req, res, next) => {
@@ -33,3 +19,33 @@ module.exports.getRandomUser = (req, res, next) => {
         });
     }
 };
+
+module.exports.getAllUsers = (req, res, next) => {
+    try {
+        const file = path.join(process.cwd(), './users.json');
+        const users = fs.readFileSync(file, "utf-8");
+        const data = JSON.parse(users);
+        res.status(200).json({ "data": data });
+    } catch (err) {
+        next({
+            status: 500,
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+};
+
+// module.exports.addNewUser = (req, res, next) => {
+//     try {
+//         const file = path.join(process.cwd(), './users.json');
+//         const users = fs.readFileSync(file, "utf-8");
+//         const data = JSON.parse(users);
+//         res.status(200).json({ "data": data });
+//     } catch (err) {
+//         next({
+//             status: 500,
+//             success: false,
+//             message: "Internal Server Error"
+//         });
+//     }
+// };
--- routes/v1/userInfo.route.js
+++ routes/v1/userInfo.route.js
@@ -3,14 +3,14 @@ const userInfoController = require('../../controllers/userInfo.controller');
 const router = express.Router();
 
 
-// GET ALL USER
-router.get('/all', userInfoController.getAllUsers);
-
 // GET A RANDOM USER
-router.get('/random', userInfoController.getRandomUser);
+router.get("/random", userInfoController.getRandomUser);
+
+// GET ALL USER
+router.get("/all", userInfoController.getAllUsers);
 
 // ADD NEW USER
-// router.get('/random', userInfoController.getRandomUser);
+router.post("/save", userInfoController.addNewUser);
 
 
 module.exports = router;
