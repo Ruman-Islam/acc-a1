4db287e5dd6b9442add614dcb77501e2e845a7e2
Ruman-Islam <rumanislam48@gmail.com>
2022-09-03 18:03:04 +0600

    bug fixed

--- controllers/userInfo.controller.js
+++ controllers/userInfo.controller.js
@@ -6,7 +6,8 @@ const getRandomIndex = require('../utilities/randomIndexGen');
 
 module.exports.getRandomUser = (req, res, next) => {
     try {
-        const data = JSON.parse(fs.readFileSync('./users.json', "utf-8"));
+        const file = path.join(process.cwd(), './users.json');
+        const data = JSON.parse(fs.readFileSync(file, "utf-8"));
         const index = getRandomIndex(data.length);
         res.status(200).json({ "data": data[index] });
     } catch (err) {
@@ -22,7 +23,7 @@ module.exports.getRandomUser = (req, res, next) => {
 module.exports.getAllUsers = (req, res, next) => {
     try {
         const { limit } = req.query;
-        const file = path.join(process.cwd(), './users.json')
+        const file = path.join(process.cwd(), './users.json');
         const data = JSON.parse(fs.readFileSync(file, "utf-8"));
         const users = limit > 0 ? data.slice(0, limit) : data;
         res.status(200).json({ "data": users });
@@ -38,9 +39,10 @@ module.exports.getAllUsers = (req, res, next) => {
 
 module.exports.addNewUser = (req, res, next) => {
     try {
-        const data = JSON.parse(fs.readFileSync('./users.json', "utf-8"));
+        const file = path.join(process.cwd(), './users.json');
+        const data = JSON.parse(fs.readFileSync(file, "utf-8"));
         data.push(req.body);
-        fs.writeFileSync("./users.json", JSON.stringify(data), "utf-8");
+        fs.writeFileSync(file, JSON.stringify(data), "utf-8");
         res.status(200).json({ "Success": true });
     } catch (err) {
         next({
@@ -56,7 +58,8 @@ module.exports.updateUser = (req, res, next) => {
     try {
         const { id } = req.params;
         const updatedData = req.body;
-        const data = JSON.parse(fs.readFileSync('./users.json', "utf-8"));
+        const file = path.join(process.cwd(), './users.json');
+        const data = JSON.parse(fs.readFileSync(file, "utf-8"));
         const newArray = data.map(user => {
             if ((user.id).toString() === id) {
                 return { ...updatedData };
@@ -64,7 +67,7 @@ module.exports.updateUser = (req, res, next) => {
                 return user;
             }
         })
-        fs.writeFileSync("./users.json", JSON.stringify(data), "utf-8");
+        fs.writeFileSync("./users.json", JSON.stringify(newArray), "utf-8");
         res.status(200).json({ "Success": true });
     } catch (err) {
         next({
